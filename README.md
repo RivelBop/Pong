f19d418a3915c02e1d37d395bee4f080774fa0d2
RivelBop <rivelbop@gmail.com>
2025-03-18 21:41:37 -0400

    Project Setup
    Gdx-Liftoff:
    * Project Name: Pong
    * Package Name: com.rivelbop.pong
    * Main Class: Pong
    * Platforms: Desktop, Core
    * Extensions: Freetype
    * Template: Classic
    * Third-Party: None
    * libGDX Version: Latest (1.13.1)
    * Java Version: 8
    * App Version: 1.0.0
    
    Changes:
    * Clear the default code.
    * Remove comments from launcher.
    * Create, store, and use 640x480 resolution.
    * Set resizable to false.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pong
+## Description
+A basic Pong clone made with libGDX, based on 1975 Atari Pong.
+
+![Pong Game](https://i.ytimg.com/vi/N4mYYGykz7E/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDGh-njhoUFNkzVCxGgniantkl2RQ)
+
+## Features
+* Player Paddle (Controlled with Arrow Keys)
+* Enemy Paddle (Controlled by AI)
+* Bouncing Ball
+* Scoreboard (For Both Player and Enemy)
+ 
+## Full Udemy Course
+Used to teach libGDX game development:
+[Udemy Profile](https://www.udemy.com/user/david-jerzak/)
---
+++ core/src/main/java/com/rivelbop/pong/Pong.java
@@ -0,0 +1,27 @@
+package com.rivelbop.pong;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/**
+ * A basic Pong clone.
+ * Used to teach libGDX game development.
+ *
+ * @see <a href="https://www.udemy.com/user/david-jerzak/">Udemy</a>
+ * @author David Jerzak
+ */
+public class Pong extends ApplicationAdapter {
+    // Window size
+    public static final int WIDTH = 640, HEIGHT = 480;
+
+    @Override
+    public void create() {
+    }
+
+    @Override
+    public void render() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
